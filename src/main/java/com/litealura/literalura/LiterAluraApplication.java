d54571aec4879b3acd54860092b2e3a5156e0563
Amir Albuquerque <amirsilva00an@gmail.com>
2025-08-19 14:22:06 -0300

    Chore: Implementando linhas de comando e menu

--- src/main/java/com/litealura/literalura/LiterAluraApplication.java
+++ src/main/java/com/litealura/literalura/LiterAluraApplication.java
@@ -1,6 +1,8 @@
 package com.litealura.literalura;
 
 import com.litealura.literalura.principal.Principal;
+import com.litealura.literalura.repository.LivroRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +10,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LiterAluraApplication implements CommandLineRunner {
 
+	@Autowired
+	private LivroRepositorio repositorio;
+
 	public static void main(String[] args) {
 		SpringApplication.run(LiterAluraApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Principal principal = new Principal();
+		Principal principal = new Principal(repositorio);
 		principal.exibeMenu();
 	}
 }
--- src/main/java/com/litealura/literalura/principal/Principal.java
+++ src/main/java/com/litealura/literalura/principal/Principal.java
@@ -1,11 +1,32 @@
 package com.litealura.literalura.principal;
 
+import com.litealura.literalura.model.DadosLivro;
+import com.litealura.literalura.model.Livro;
+import com.litealura.literalura.repository.LivroRepositorio;
+import com.litealura.literalura.service.ConsumoAPI;
+import com.litealura.literalura.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
 
+    private final String urlBase = "https://gutendex.com/books/?search=";
+
     private Integer opcao = -1;
     private Scanner scanner = new Scanner(System.in);
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private ConverteDados converteDados = new ConverteDados();
+    private LivroRepositorio livroRepositorio;
+
+    //Variável Global
+    private String livroBuscado;
+    private List<Livro> livros = new ArrayList<>();
+
+    public Principal(LivroRepositorio repositorio) {
+        this.livroRepositorio = repositorio;
+    }
 
     private String menu = """
             Escolha uma das opções abaixo:
@@ -22,8 +43,44 @@ public class Principal {
     public void exibeMenu() {
         while (opcao != 0) {
             System.out.println(menu);
-            var escolha = scanner.nextInt();
+            opcao = scanner.nextInt();
             scanner.nextLine();
+
+            switch (opcao) {
+
+                case 1:
+                    buscarLivroPeloTitulo();
+                    break;
+                case 2:
+                    buscarLivro();
+                    break;
+                case 0:
+                    System.out.println("Finalizando...");
+                    break;
+            }
         }
-    };
+    }
+
+    private List<DadosLivro> obterLivro() {
+        System.out.println("Informe o nome do livro buscado: ");
+        livroBuscado = scanner.nextLine();
+        String url = urlBase + livroBuscado.replace(" ", "+");
+        var json = consumoAPI.obterDados(url);
+        return converteDados.obterDados(json, DadosLivro.class);
+    }
+
+    private void buscarLivroPeloTitulo() {
+        Livro livro = new Livro(obterLivro());
+        if (livro == null){
+            System.out.println("Nenhum livro encontrado");
+            return;
+        }
+        livroRepositorio.save(livro);
+        System.out.println("Livro cadastrado no banco de dados com sucesso!");
+    }
+
+    private void buscarLivro() {
+        livros = livroRepositorio.findAll();
+        livros.forEach(System.out::println);
+    }
 }
